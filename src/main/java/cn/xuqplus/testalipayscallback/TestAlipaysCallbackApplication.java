5280d0d94bec85837bd8ffddf8ce2ae7d12824e1
xjplus <xxjplus@github.com>
2019-03-21 13:53:06 +0800

    .

--- src/main/java/cn/xuqplus/testalipayscallback/TestAlipaysCallbackApplication.java
+++ src/main/java/cn/xuqplus/testalipayscallback/TestAlipaysCallbackApplication.java
@@ -4,10 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 @SpringBootApplication
@@ -26,9 +31,19 @@ public class TestAlipaysCallbackApplication {
     return "ok";
   }
 
-  @RequestMapping("callback")
-  public String callback(@RequestBody Map data) {
-    data.forEach((k, v) -> LOGGER.info("key={}, val={}", k, v));
-    return "ok";
+  @GetMapping("callback")
+  public String callback(String app_id, String scope, String auth_code) {
+    LOGGER.info("app_id={}, scope={}, auth_code={}, ", app_id, scope, auth_code);
+    return String.format("app_id=%s, scope=%s, auth_code=%s, ", app_id, scope, auth_code);
+  }
+
+  @GetMapping("auth")
+  public ModelAndView auth(ModelAndView mav, HttpServletRequest request) throws UnsupportedEncodingException {
+    String remoteAddr = request.getRequestURL().substring(0, request.getRequestURL().length() - request.getRequestURI().length());
+    String callback = URLEncoder.encode("http://xjplus.xyz:8090/callback", "utf8");
+    mav.setViewName(String.format("redirect:https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=%s&scope=auth_base&redirect_uri=%s",
+            "2018080760954356",
+            callback));
+    return mav;
   }
 }
