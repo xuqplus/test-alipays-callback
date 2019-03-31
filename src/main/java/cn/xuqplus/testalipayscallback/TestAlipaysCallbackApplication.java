a3688a6d777ac5aff064cdd77561083061008312
xjplus <xxjplus@github.com>
2019-03-31 16:42:22 +0800

    done

--- src/main/java/cn/xuqplus/testalipayscallback/TestAlipaysCallbackApplication.java
+++ src/main/java/cn/xuqplus/testalipayscallback/TestAlipaysCallbackApplication.java
@@ -49,8 +49,8 @@ public class TestAlipaysCallbackApplication {
           "xuqplus的创意工坊",
           "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoB3a1OPeLkhrNpGNZ2gcfyHBany5UMNbwxjRVGlwxcnQIEUwIe7vVJz4Vl277J4BBuuxBZwgp43/4OAs450D0doECzJ1nXz070aylTkalIrsqjuD8dY2fdjO0Msi7uYGuPmgXHke2GjxLLYHWsSKhMyjVYkOgQkYzKQbj5XDJmVrVh3yzeUu7hzb3a+a7cBNVk0sRqC16NKU38Y4nNAZgsgZn+p/rs39CxItqca2/qpdcmFJWPOtJZIE06iZ3OZeTtqx2CK+uUN8sgG0PL2k8iqSV2naSFiaiQT3omwjZOwOY/RHqtZFpMcK0Pnew1GnS7f99uGI0YDrZDA399kzrQIDAQAB",
           "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQChuIgbxxxaOxIHXsQrQJB/4lqjTYlr5YGG2+IPzUMO//Ar/wiUMa5XJi3OxsoKm+JCRDrsIu7Oag3cfuo1W6jAbCwPzFcV3WkezB7MgDr/q/Do1nkgLHqk8N3OUjhQ8VnD1dX9spnMyHJkk0Zzv3W0ahFjGdWZpJg8b6WQ+SHb/PVXU9OMImUZ8rcQhJbFfPsXMPdSbGWW7fzOqjCd1J8UQOKfQ3auIlwzbR7mDliZyys2A5U4u+MeGOErNnDmM6nekQkryYMcOy1HD4+H7CNl1se+9GQsaMEHd+uB5LmTtkFBvyDTKZG/IzEt8pR/6FiaTFgMmFntFT7XFxP4kgq3AgMBAAECggEAYeVKv0Xwq85GQ5/iMCyNTFzBcUlmfqIXnoNMfTRL+7CCup9PmiXY23iJEjrT40gp1uNmp7sdxGX+rhNFw7cQmCrjIU0ZCmtVTwdRPYoCLPLwioIKIKfMNjx4iITNAnKXXCz9tO2AnK5aUPe2Fis4HVZ33/2rh65TWYJcPz4QeRHZORxkw5FDjUq5z8OAjM/RLNxWjcC491lKqs3xP8pFeocpQKS79STy5BaIYg3t0C5FsWpVYNjl+eN9TGbsiVurPXTQhZoE2WVYhtRIuGm2IM4pj84+7IIe2nmhWkUNYIwhgLB/HuZrW0WSm7uW3/O85eD4+CdrRWR1VEr1bn1eQQKBgQDNDYzv6nf/tb/yTOjURlF9504mpOxgrDWgeiHgMju52J8w3QlO9Zetu9ROnxHhqjjEtRFR13GaT4b5bNVvJpow8C3OqZxvwWbF0gaIr/bfF6YGgfgoZTEKhSk+1PA7ZP9wBWLl0AEU19XgkPCIqO0l/fKBxb/TIN7Uy56UPOcR3QKBgQDJ5tSVRH+N7M4BdQDjRolnoURPPp4s7KrShWpzIDRZ0auvVWsQem7bN8gFIsHss2eD4R7SaF+p/uFBaf/LSLWuQDXQJh2Fa5fWa5vYqXs9HRGpjmWAa19+KIgA2RpwNUK0CZoGXEafETbSILXvyRPk2wkN0O+HSGuEXitc040nowKBgDOo6Uv8V1Ud9gjWZYYtXRSKNL85p6/HUw9mFajFHxT4RUrrNRAmgZssihVwr/GZkAnG0GE9FQraZFfsmm0RXaFcsT2yr43cPMcz3Z+MLXcRHyvejKWosM1EtRp2TTu5ez0+ribV25/jrKeR/HeBj5WVwAX/yw8m5dzYm9Ae89wlAoGAIa59Zk1gDhgkIcAYHtIkmAIiOCoVDx+IO8iAAqartRQyVPSmAMC7/5wlpR2wRkGDkk+a6bT5yI05nMUHJC2ECYays3+4Swqw8NdQz2suoPpZePTFYBAJhS1Tyvhs3a007H3xkQAR0/V2NMW6ND9SL0qPDxv86Y+eSB3BlCHCmYsCgYAGo1Gik9SPpCvM9tKeq5Y3MnOv1966kdtJiSJecRJMon6twKBIRj4a21poNUqcRhzVpk0AOPvMpSfuLCETsFva97n8qCe3Ttgq5Cvk9wtuVi1a2JL3sRzGmGSvZik27cBJOJCvLIMgzSBHr6Ct+IXOt5s7F9/9Tb/ry8xo+6ZKgg==",
-          "http://106.12.80.76:8090/",
-          "http://106.12.80.76:8090/callback/");
+          "http://192.168.1.5:8090/",
+          "http://192.168.1.5:8090/callback/");
 
   private static final String getRemoteAddr(HttpServletRequest request) {
     String XForwardedFor = request.getHeader("X-Forwarded-For");
@@ -122,7 +122,6 @@ public class TestAlipaysCallbackApplication {
         LOGGER.info("auth_code={}, 换取userId失败, e.message={}, e.getErrMsg={}", auth_code, e.getMessage(), e.getErrMsg());
       }
     }
-    mav.setViewName("query");
     mav.addObject("no", no);
     if (null == order[6]) {
       Object[] merchant = (Object[]) order[4];
@@ -130,6 +129,7 @@ public class TestAlipaysCallbackApplication {
       mav.addObject("userId", merchant[1]);
       mav.addObject("loginId", merchant[0]);
     }
+    mav.setViewName("query");
     return mav;
   }
 
@@ -143,17 +143,18 @@ public class TestAlipaysCallbackApplication {
   }
 
   @GetMapping("order")
-  public Object order(String no, String amount, String memo, ModelAndView mav, HttpServletRequest request) throws Exception {
-    LOGGER.info("order no={}, amount={}, memo={}", no, amount, memo);
-    if (StringUtils.isEmpty(no)) {
+  public Object order(String no, String amount, String desc, ModelAndView mav, HttpServletRequest request) throws Exception {
+    LOGGER.info("order no={}, amount={}, desc={}", no, amount, desc);
+    if (StringUtils.isEmpty(no) && !StringUtils.isEmpty(amount)) {
       /* 创建 */
       long createAt = System.currentTimeMillis();
       no = String.format("no_%s_%s", createAt, nextInt(1000000));
+
       if (orders.containsKey(no)) {
         throw new Exception("no重复, 创建失败");
       }
       Object[] merchant = merchants.get(merchants.keySet().toArray()[nextInt(merchants.keySet().size())]);
-      orders.put(no, new Object[]{no, amount, memo, createAt, merchant, null, null, null});
+      orders.put(no, new Object[]{no, amount, desc, createAt, merchant, null, null, null});
       mav.setViewName(String.format("redirect:https://render.alipay.com/p/s/i?scheme=%s",
               URLEncoder.encode(String.format("alipays://platformapi/startapp?saId=10000007&qrcode=%s",
                       URLEncoder.encode(String.format("%s/publicAppAuthorize?no=%s", getRemoteAddr(request), no), "utf-8")), "utf-8")));
@@ -169,7 +170,7 @@ public class TestAlipaysCallbackApplication {
     // 未绑定付款用户
     Object userId = order[5];
     Object aliTradeNo = order[6];
-    Boolean ok = (Boolean) order[7];
+    Boolean payed = (Boolean) order[7];
     if (StringUtils.isEmpty(userId)) {
       String data = String.format("redirect:https://render.alipay.com/p/s/i?scheme=%s",
               URLEncoder.encode(String.format("alipays://platformapi/startapp?saId=10000007&qrcode=%s",
@@ -179,7 +180,7 @@ public class TestAlipaysCallbackApplication {
     if (StringUtils.isEmpty(aliTradeNo)) {
       return new Res("2", "wait", null);
     }
-    if (ok) {
+    if (null != payed && payed) {
       return new Res("3", "payed", null);
     }
     return new Res("0", "ok", aliTradeNo);
@@ -192,18 +193,18 @@ public class TestAlipaysCallbackApplication {
     String no = users.get(userId);
     Object[] order = orders.get(no);
     Object amount = order[1];
-    Object memo = order[2];
+    Object desc = order[2];
     Object createAt = order[3];
     Object[] merchant = (Object[]) order[4];
     Object userId0 = order[5];
     Object aliTradeNo = order[6];
     if (null == aliTradeNo) {
       Map r = new HashMap();
+      r.put("no", no);
       r.put("amount", amount);
-      r.put("memo", memo);
+      r.put("desc", desc);
       r.put("createAt", createAt);
       r.put("merchant", merchant);
-      r.put("userId0", userId0);
       r.put("userId", userId);
       return new Res("0", "ok", r);
     }
@@ -218,9 +219,8 @@ public class TestAlipaysCallbackApplication {
   }
 
   @GetMapping("notify")
-  public Object notify(String no, String aliTradeNo) {
-    LOGGER.info("notify no={}, aliTradeNo={}", no, aliTradeNo);
-    orders.get(no)[7] = true;
+  public Object notify( String aliTradeNo) {
+    LOGGER.info("notify aliTradeNo={}", aliTradeNo);
     return new Res("0", "ok", null);
   }
 }
