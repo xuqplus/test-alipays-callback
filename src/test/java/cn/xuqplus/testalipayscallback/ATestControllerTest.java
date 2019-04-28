a09c9a6b6922ad402c10970c2745d68b743e1ee3
xjplus <xxjplus@github.com>
2019-04-28 21:25:08 +0800

    3

---
+++ src/test/java/cn/xuqplus/testalipayscallback/ATestControllerTest.java
@@ -0,0 +1,24 @@
+package cn.xuqplus.testalipayscallback;
+
+import org.junit.Test;
+
+public class ATestControllerTest {
+
+  @Test
+  public void aa() {
+    // test
+  }
+
+  @Test
+  public void bb() {
+    // test
+	// 3
+	// 2
+	// 1
+  }
+
+  @Test
+  public void cc() {
+    // test
+  }
+}
