22b3731eb0768df9d093fb81b928a4414071e303
xjplus <xxjplus@github.com>
2019-04-28 21:23:48 +0800

    add two methods

---
+++ src/test/java/cn/xuqplus/testalipayscallback/ATestControllerTest.java
@@ -0,0 +1,16 @@
+package cn.xuqplus.testalipayscallback;
+
+import org.junit.Test;
+
+public class ATestControllerTest {
+
+  @Test
+  public void aa() {
+    // test
+  }
+
+  @Test
+  public void bb() {
+    // test
+  }
+}
